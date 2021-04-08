0fc941b9ff506bffa50c4f80e18aad07e5bea624
Narayan Sainaney <narayan@sainaney.com>
2021-04-07 17:16:57 -0700

    Backend using app-engine and mongo service

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,16 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-var monngoURL = 'mongodb://mongo-service/database'
+const monngoURL = process.env.BACKEND_MONGO_CONNECTION //'mongodb://mongo-svc/database'
 
 app.use(cors({
     origin: '*'
 }))
 
-
+let retVal = 'not connected yet'
 app.get('/api', (req, res) => {
     res.send({
-        "data": "hello world!"
+        "data": retVal
     })
 })
 
@@ -21,8 +21,10 @@ mongoose.connect(monngoURL, { useNewUrlParser: true })
     .then(
         () => {
             console.log("connected to mongo");
+            retVal = 'YAY - connected to mongo'
         }
     ).catch((error) => {
+
         console.log("unable to connect to mongoDB")
         console.log(monngoURL);
     });
