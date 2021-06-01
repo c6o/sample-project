10b2ff6406a6b6d1497f9dd01dc33cda9464f941
Robb Lovell <robb@codezero.io>
2021-06-01 11:10:12 -0700

    updated NGINX configuration to include sockets in the redirect.

--- halyard-frontend/nginx-start.sh
+++ halyard-frontend/nginx-start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -eu
-echo URL: ${HALYARD_API_HOST}:${HALYARD_API_PORT}
-envsubst '${HALYARD_API_HOST} ${HALYARD_API_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf && \
+echo BACKEND_URL: ${HALYARD_API_HOST}:${HALYARD_API_PORT}
+echo SOCKETS_URL: ${HALYARD_SOCKETS_HOST}:${HALYARD_SOCKETS_PORT}
+envsubst '${HALYARD_API_HOST} ${HALYARD_API_PORT} ${HALYARD_SOCKETS_HOST} ${HALYARD_SOCKETS_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf && \
 
 exec "$@"
