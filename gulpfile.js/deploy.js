f707b8f580312e7abc33174d66b595f2ee2036cb
Robb Lovell <robb@codezero.io>
2021-12-14 12:54:36 -0800

    reverted now that the key is encoded

--- gulpfile.js/deploy.js
+++ gulpfile.js/deploy.js
@@ -54,7 +54,12 @@ const apply_gcloud = async (environment) => {
     const project = PROJECT_LOOKUP[environment]
     if (dryRun())
         console.log(`\x1b[1m\x1b[35mThis is a dry run\x1b[0m`)
-
+    // base64 decode the GCLOUD KEY = require(the environment variable set in the CICD job runner.
+    let data = `${process.env.GCLOUD_KEY}`
+    let buff = Buffer.from(data, 'base64')
+    let text = buff.toString('ascii')
+    // write the key to the home directory's gcloud.json file.
+    writeFileSync(`${process.env.GCLOUD_KEY_FILE}`, text)
     // authenticate to gcloud
     await spawner(`gcloud auth activate-service-account --key-file=${process.env.GCLOUD_KEY_FILE}`, false, true)
     await spawner(`gcloud container clusters get-credentials hub --zone ${ZONE} --project ${project}`, false, true)
@@ -87,19 +92,19 @@ const apply = async (environment) => {
 
     // Authentication
     if (onBuildServer()) {
-        if (!process.env.GCLOUD_KEY_FILE && (!process.env.KUBECONFIG_CERT_AUTH_DATA || !process.env.KUBECONFIG_SERVER || !process.env.KUBECONFIG_USER_TOKEN)) {
+        if (!process.env.GCLOUD_KEY && (!process.env.KUBECONFIG_CERT_AUTH_DATA || !process.env.KUBECONFIG_SERVER || !process.env.KUBECONFIG_USER_TOKEN)) {
             throw new GulpError('apply', new Error('Error: If deploying with a kubeconfig, three environment variables are required for build server deployments from a valid kubeconfig.yaml file:\n' +
                 'KUBECONFIG_CERT_AUTH_DATA (from clusters:- cluster: certificate-authority-data:),\n' +
                 'KUBECONFIG_SERVER (from clusters:- cluster: server:), and \n' +
                 'KUBECONFIG_USER_TOKEN (from users: user: token:)\n' +
-                'If deploying to google cloud, you need to set a GCLOUD_KEY_FILE environment variable.'))
+                'If deploying to google cloud, you need to set a GCLOUD_KEY environment variable.'))
         }
     } else {
-        if (!process.env.GCLOUD_KEY_FILE && !process.env.KUBECONFIG) {
-            throw new GulpError('apply', new Error('Error: If deploying with a kubeconfig file, the KUBECONFIG environment variable must be set to a valid kubeconfig.yaml file, or a GCLOUD_KEY_FILE should be set.'))
+        if (!process.env.GCLOUD_KEY && !process.env.KUBECONFIG) {
+            throw new GulpError('apply', new Error('Error: If deploying with a kubeconfig file, the KUBECONFIG environment variable must be set to a valid kubeconfig.yaml file, or a GCLOUD_KEY should be set.'))
         }
     }
-    if (process.env.GCLOUD_KEY_FILE) {
+    if (process.env.GCLOUD_KEY) {
         await apply_gcloud(environment)
     } else {
         await apply_kubernetes(environment)
